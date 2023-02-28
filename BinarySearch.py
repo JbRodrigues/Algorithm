ed2d0b394114130284fff28c318b02842f8495aa
JbRodrigues <juliobisaia@hotmail.com>
2023-02-28 16:29:45 -0300

    Fix some issues with variables, now it apresents stable bahavior

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,29 +1,33 @@
+import math
 from random import randint
 
 
-guess = int(input("Guess a number between 1 and 15 "))
-number = randint(1,15)
+
+#define a random number between 1 and 100
+number = randint(1,100)
 atempt = 0
-minimum = 1
-maximum = number
-guessing = bool(True)
-
-
-while guessing != bool(False):
-
+minn = 1
+maxx = 100
+midd = math.floor(int(minn + maxx) / 2)
+print(str(number))
+guess = int(input("Guess a number between 1 and 100 "))
+while minn <  maxx:
+    atempt += 1
+    print("Range " + str(minn) + " and " + str(maxx))
     if guess == number:
-        atempt = 1;
-        print("You Got it, the number is: " + str (number))
-        guessing = bool(False)
+        print("You Got it, with " + str(atempt) + " atempts")
+        break
     else:
         if(guess > number):
-            maximum = guess - 1
-            average = int((minimum  + maximum) / 2)
-            guess = int(input("Too high, guess again between "+str(minimum) +"and"+str(average)+" "))
+            maxx = midd
+            print("Tentativa " + str(atempt))
+            midd = math.floor(int(minn + maxx) / 2)
+            guess = int(input("Too high, Try " + str(midd) + " "))
         elif(guess < number):
-            minimum = guess + 1
-            average = int((minimum  + maximum) / 2)
-            guess = int(input("Too low, guess again between "+str(average) +" and "+str(maximum)+" "))
+            minn = midd
+            midd = math.floor(int(minn + maxx) / 2)
+            print("Tentativa " + str(atempt))
+            guess = int(input("Too Low, Try " + str(midd) + " "))
 
 
 
