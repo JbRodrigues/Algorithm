1dd6cfa8ffbfa87472b565a10beed63feab3c3f4
JbRodrigues <juliobisaia@hotmail.com>
2023-02-28 16:51:37 -0300

    Fix variable behavior

--- BinarySearch.py
+++ BinarySearch.py
@@ -5,29 +5,27 @@ from random import randint
 
 #define a random number between 1 and 100
 number = randint(1,100)
-atempt = 0
+attempt = 0
 minn = 1
 maxx = 100
-midd = math.floor(int(minn + maxx) / 2)
-print(str(number))
+#set the middle of range
 guess = int(input("Guess a number between 1 and 100 "))
 while minn <  maxx:
-    atempt += 1
-    print("Range " + str(minn) + " and " + str(maxx))
+    attempt += 1
+    if(guess > number):
+        maxx = guess
+        midd = math.floor(int(minn + maxx) / 2)
+        print("Attempt " + str(attempt))
+        guess = int(input("Too high, Try " + str(midd) + " "))
+    elif(guess < number):
+        minn = guess
+        midd = math.floor(int(minn + maxx) / 2)
+        print("Attempt " + str(attempt))
+        guess = int(input("Too Low, Try " + str(midd) + " "))
     if guess == number:
-        print("You Got it, with " + str(atempt) + " atempts")
+        print("You Got it, with " + str(attempt) + " attempts")
         break
-    else:
-        if(guess > number):
-            maxx = midd
-            print("Tentativa " + str(atempt))
-            midd = math.floor(int(minn + maxx) / 2)
-            guess = int(input("Too high, Try " + str(midd) + " "))
-        elif(guess < number):
-            minn = midd
-            midd = math.floor(int(minn + maxx) / 2)
-            print("Tentativa " + str(atempt))
-            guess = int(input("Too Low, Try " + str(midd) + " "))
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Algorithms
+# Algorithms
 
 Algorithms exercises
