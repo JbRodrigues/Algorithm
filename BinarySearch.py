1a80952046e66c86f078ac98a93ef9668897a3b9
JbRodrigues <juliobisaia@hotmail.com>
2023-02-28 16:58:53 -0300

    Modify readme

--- BinarySearch.py
+++ BinarySearch.py
@@ -11,14 +11,17 @@ maxx = 100
 #set the middle of range
 guess = int(input("Guess a number between 1 and 100 "))
 while minn <  maxx:
+    #number of attempts (The max must be 7)
     attempt += 1
     if(guess > number):
         maxx = guess
+        #define new middle of range
         midd = math.floor(int(minn + maxx) / 2)
         print("Attempt " + str(attempt))
         guess = int(input("Too high, Try " + str(midd) + " "))
     elif(guess < number):
         minn = guess
+        #define new middle of range
         midd = math.floor(int(minn + maxx) / 2)
         print("Attempt " + str(attempt))
         guess = int(input("Too Low, Try " + str(midd) + " "))
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Algorithms
 
 Algorithms exercises
+
+To Run this algorithms, you must have Python downloded on your machine, then, download files with `.py` extention.
+Enter on the folder with your python file and execute `python name-file.py` and follow the structions.
